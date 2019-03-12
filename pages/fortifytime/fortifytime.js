276a5cbca7ae98dcf37a554061c82f305d33e324
yangalong <172778564@qq.com>
2019-03-12 14:06:33 +0800

    删除不必要的图片，压缩小程序体积

--- pages/fortifytime/fortifytime.js
+++ pages/fortifytime/fortifytime.js
@@ -11,9 +11,40 @@ Page({
         someData: {
             statusBarHeight: app.globalData.statusBarHeight,
             titleBarHeight: app.globalData.titleBarHeight
-        }
+        },
+        checkboxItems: [
+            {name: '工作日', value: '0'},
+            {name: '周末', value: '1'},
+            {name: '每天', value: '2'}
+        ],
+        items: [
+            { name: '工作日', value: '0' },
+            { name: '周末', value: '1' },
+            { name: '每天', value: '2' }
+        ]
     },
+    checkboxChange: function (e) {
+        console.log('checkbox发生chang事件', e.detail.value)
+    },
+    radioChange: function (e) {
+        console.log('checkbox发生change事件，携带value值为：', e.detail.value);
 
+        var checkboxItems = this.data.checkboxItems, values = e.detail.value;
+        for (var i = 0, lenI = checkboxItems.length; i < lenI; ++i) {
+            checkboxItems[i].checked = false;
+
+            for (var j = 0, lenJ = values.length; j < lenJ; ++j) {
+                if(checkboxItems[i].value == values[j]){
+                    checkboxItems[i].checked = true;
+                    break;
+                }
+            }
+        }
+
+        this.setData({
+            checkboxItems: checkboxItems
+        });
+    },
     /**
      * 生命周期函数--监听页面加载
      */
