25a727dab06d77e2a2ced9a19af3b8daa9a3c9ae
yangalong <172778564@qq.com>
2019-03-12 16:46:41 +0800

    设防时间

--- pages/fortifytime/fortifytime.js
+++ pages/fortifytime/fortifytime.js
@@ -21,10 +21,20 @@ Page({
             { name: '工作日', value: '0' },
             { name: '周末', value: '1' },
             { name: '每天', value: '2' }
-        ]
+        ],
+        array: ['0:00', '1:00', '2:00', '3:00','4:00', '5:00', '6:00', '7:00','8:00', '9:00', '10:00', '11:00','12:00', '13:00', '14:00', '15:00', '16:00','17:00', '18:00', '19:00', '20:00','21:00', '22:00', '23:00', '24:00'],
+    },
+    bindPickerChange(e) {
+        console.log('picker发送选择改变，携带值为', e.detail.value)
+        this.setData({
+            index: e.detail.value
+        })
     },
     checkboxChange: function (e) {
         console.log('checkbox发生chang事件', e.detail.value)
+    },
+    switch1Change(e) {
+        console.log('switch1 发生 change 事件，携带值为', e.detail.value)
     },
     radioChange: function (e) {
         console.log('checkbox发生change事件，携带value值为：', e.detail.value);
