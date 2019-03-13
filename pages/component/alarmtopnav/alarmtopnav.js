4944416265cd2ee59e34e6e229ff61cc34c7c065
yangalong <172778564@qq.com>
2019-03-13 14:34:03 +0800

    修改自定义顶部导航栏

--- pages/component/alarmtopnav/alarmtopnav.js
+++ pages/component/alarmtopnav/alarmtopnav.js
@@ -24,65 +24,44 @@ Component({
           titleBarHeight: app.globalData.titleBarHeight
       }
   },
-    //扫一扫
-    click: function (event) {
-        var that = this;
-        var show;
-        wx.scanCode({
-            success: (res) => {
-                this.show = "结果:" + res.result + "二维码类型:" + res.scanType + "字符集:" + res.charSet + "路径:" + res.path;
-                that.setData({
-                    show: this.show
-                })
-                wx.showToast({
-                    title: '成功',
-                    icon: 'success',
-                    duration: 2000
-                })
-            },
-            fail: (res) => {
-                wx.showToast({
-                    title: '失败',
-                    icon: 'success',
-                    duration: 2000
-                })
-            },
-            complete: (res) => {
-            }
-        })
-    },
 
   onLoad: function (options) {
-      // var that = this
-      // wx.getSystemInfo({
-      //     success: function (res) {
-      //         that.globalData.platform = res.platform
-      //         let totalTopHeight = 68
-      //         if (res.model.indexOf('iPhone X') !== -1) {
-      //             totalTopHeight = 88
-      //         } else if (res.model.indexOf('iPhone') !== -1) {
-      //             totalTopHeight = 64
-      //         }
-      //         that.globalData.statusBarHeight = res.statusBarHeight
-      //         that.globalData.titleBarHeight = totalTopHeight - res.statusBarHeight
-      //     },
-      //     failure() {
-      //         that.globalData.statusBarHeight = 0
-      //         that.globalData.titleBarHeight = 0
-      //     }
-      // })
+
    },
 
   methods: {
-    backHome: function () {
-      wx.reLaunch({
-        url: '../index/index',
-      })
-    },
+    //返回
     back: function () {
       wx.navigateBack({
         delta: 1
       })
-    }
+    },
+    //扫一扫
+    clicks: function (event) {
+          var that = this;
+          wx.scanCode({
+              success: (res) => {
+                  this.show = "结果:" + res.result + "二维码类型:" + res.scanType + "字符集:" + res.charSet + "路径:" + res.path;
+                  that.setData({
+                      show: this.show
+                  })
+                  wx.showToast({
+                      title: '成功',
+                      icon: 'success',
+                      duration: 2000
+                  })
+              },
+              fail: (res) => {
+                  wx.showToast({
+                      title: '失败',
+                      icon: 'success',
+                      duration: 2000
+                  })
+              },
+              complete: (res) => {
+              }
+          })
+      },
+
   }
 })
--- pages/component/navbar/navbar.js
+++ pages/component/navbar/navbar.js
@@ -25,32 +25,10 @@ Component({
       }
   },
   onLoad: function (options) {
-      // var that = this
-      // wx.getSystemInfo({
-      //     success: function (res) {
-      //         that.globalData.platform = res.platform
-      //         let totalTopHeight = 68
-      //         if (res.model.indexOf('iPhone X') !== -1) {
-      //             totalTopHeight = 88
-      //         } else if (res.model.indexOf('iPhone') !== -1) {
-      //             totalTopHeight = 64
-      //         }
-      //         that.globalData.statusBarHeight = res.statusBarHeight
-      //         that.globalData.titleBarHeight = totalTopHeight - res.statusBarHeight
-      //     },
-      //     failure() {
-      //         that.globalData.statusBarHeight = 0
-      //         that.globalData.titleBarHeight = 0
-      //     }
-      // })
+
    },
 
   methods: {
-    backHome: function () {
-      wx.reLaunch({
-        url: '../index/index',
-      })
-    },
     back: function () {
       wx.navigateBack({
         delta: 1
