08d8de73e52fd61ed15ac6905ef60af6bddf113c
yangalong <172778564@qq.com>
2019-03-13 09:36:25 +0800

    设备列表模拟数据

--- pages/equip/equip.js
+++ pages/equip/equip.js
@@ -11,7 +11,66 @@ Page({
         someData: {
             statusBarHeight: app.globalData.statusBarHeight,
             titleBarHeight: app.globalData.titleBarHeight
-        }
+        },
+        //测试模拟数据
+        equipListData: [
+            {
+                "alarmtype": "入侵报警",
+                "alarmtime": "2019-03-12 18:01:38",
+                "equip": "yy",
+                "address": "60",
+                "imgUrl": "http://pic01.aokecloud.cn/alarm/1000004/pic/20190119/1000004_20190119104637_320X240.jpg",
+            },
+            {
+                "alarmtype": "整点打卡",
+                "alarmtime": "2019-02-12 12:43:38",
+                "equip": "西安",
+                "address": "人",
+                "imgUrl": "http://pic01.aokecloud.cn/alarm/1000019/pic/20190312/EFGABC019_20190312182043_320X180.jpg",
+            },
+            {
+                "alarmtype": "入侵报警",
+                "alarmtime": "2019-03-12 18:01:38",
+                "equip": "yy",
+                "address": "咸阳",
+                "imgUrl": "http://pic01.aokecloud.cn/alarm/1000004/pic/20190119/1000004_20190119104637_320X240.jpg",
+            },
+            {
+                "alarmtype": "整点打卡",
+                "alarmtime": "2019-02-12 12:43:38",
+                "equip": "西安",
+                "address": "人",
+                "imgUrl": "http://pic01.aokecloud.cn/alarm/1000019/pic/20190312/EFGABC019_20190312180148_320X180.jpg",
+            },
+            {
+                "alarmtype": "入侵报警",
+                "alarmtime": "2019-03-12 18:01:38",
+                "equip": "yy",
+                "address": "渭南",
+                "imgUrl": "http://pic01.aokecloud.cn/alarm/1000019/pic/20190312/EFGABC019_20190312175426_320X180.jpg",
+            },
+            {
+                "alarmtype": "整点打卡",
+                "alarmtime": "2019-03-12 16:53:39",
+                "equip": "西安",
+                "address": "人",
+                "imgUrl": "http://pic01.aokecloud.cn/alarm/1000019/pic/20190312/EFGABC019_20190312172013_320X180.jpg",
+            },
+            {
+                "alarmtype": "入侵报警",
+                "alarmtime": "2019-03-12 16:50:03",
+                "equip": "yy",
+                "address": "雁塔",
+                "imgUrl": "http://pic01.aokecloud.cn/alarm/1000019/pic/20190312/EFGABC019_20190312163744_320X180.jpg",
+            },
+            {
+                "alarmtype": "整点打卡",
+                "alarmtime": "2019-03-12 16:37:44",
+                "equip": "西安",
+                "address": "灞桥",
+                "imgUrl": "http://pic01.aokecloud.cn/alarm/1000019/pic/20190312//EFGABC019_20190312165339.jpg",
+            },
+        ]
     },
     //跳转详情页
     changeToequipdetail:function(){
