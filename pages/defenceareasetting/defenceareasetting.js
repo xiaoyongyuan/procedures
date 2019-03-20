7a01460c3e4b0840e0611c2e89c044518b8c1937
yangalong <172778564@qq.com>
2019-03-20 17:19:52 +0800

    防区取整

--- pages/defenceareasetting/defenceareasetting.js
+++ pages/defenceareasetting/defenceareasetting.js
@@ -18,7 +18,8 @@ Page({
         pointlist:[],
         addfield:true,
         reddisplay:'none',
-        addone:false
+        addone:false,
+        bluedisplay:'block'
     },
     //页面跳转
     changeTosettingequipinfo:function(){
@@ -32,6 +33,9 @@ Page({
     onLoad: function (options) {
         var that = this;
         var code = options.currentcode;
+        that.setData({
+            currentcode:code
+        });
         /**
          * 请求设备详情接口
          */
@@ -69,6 +73,9 @@ Page({
                     myblue_carvas.stroke();//画出当前路径的边框
                     myblue_carvas.draw(); //将之前在绘图上下文中的描述（路径、变形、样式）画到 canvas 中。
                 }
+                /**
+                 * 点进来加载红色防区
+                 */
                 if(that.data.field[2] !== undefined){
                     const x1 = that.data.field[2][0][0][0];
                     const y1 = that.data.field[2][0][0][1];
@@ -113,8 +120,10 @@ Page({
      */
     EventHandleStartblue:function(e){
         var that = this;
-        strat_xblue = e.touches[0].x; // 手指开始触摸时的x轴 x轴--->相对于画布左边的距离
-        strat_yblue = e.touches[0].y;// 手指开始触摸时的y轴 y轴--->相对于画布顶部的距离
+        strat_xblue = Math.round(e.touches[0].x); // 手指开始触摸时的x轴 x轴--->相对于画布左边的距离
+        strat_yblue = Math.round(e.touches[0].y);// 手指开始触摸时的y轴 y轴--->相对于画布顶部的距离
+        // strat_yblue = Math.round(e.touches[0].y)
+        console.log("e",e);
         console.log("strat_xblue",strat_xblue,strat_yblue);
     },
     /**
@@ -122,8 +131,8 @@ Page({
      */
     EventHandleblue:function(e){
         var that = this;
-        end_xblue = e.changedTouches[0].x; // 手指结束触摸时的x轴 x轴--->相对于画布左边的距离
-        end_yblue = e.changedTouches[0].y;// 手指结束触摸时的y轴 y轴--->相对于画布顶部的距离
+        end_xblue = Math.round(e.changedTouches[0].x); // 手指结束触摸时的x轴 x轴--->相对于画布左边的距离
+        end_yblue = Math.round(e.changedTouches[0].y);// 手指结束触摸时的y轴 y轴--->相对于画布顶部的距离
 
         /**
          * 两个防区都没有
@@ -133,7 +142,7 @@ Page({
                 const datapoint=that.data.pointlist;
                 var point1 = [strat_xblue,strat_yblue];
                 var point2 = [end_xblue,end_yblue];
-                if(that.data.dianlist.length < 6) {
+                if(that.data.pointlist.length < 6) {
                     datapoint.push(point1,point2);
                 }
                 that.setData({
@@ -173,7 +182,10 @@ Page({
         });
         console.log("kaishi",that.data.dianlist);
 
-        if(that.data.field[2] === undefined){
+        /**
+         * 有1没有2
+         */
+        if(that.data.field[1] !== undefined && that.data.field[2] === undefined){
             my_carvas.beginPath(); //创建一条路径
             my_carvas.setStrokeStyle('red');  //设置边框为红色
             my_carvas.moveTo(that.data.dianlist[0][0],that.data.dianlist[0][1]); //描述路径的起点为手指触摸的x轴和y轴
@@ -200,11 +212,29 @@ Page({
             reddisplay:'block',
             addone:!that.data.addone
         });
-        wx.showToast({
-            title: '请绘制防区1',
-            icon: 'none',
-            duration: 2000
-        });
+        /**
+         * 两个防区都没有
+         */
+        if(that.data.field[1] === undefined && that.data.field[2] === undefined){
+            wx.showToast({
+                title: '请绘制防区1',
+                icon: 'none',
+                duration: 2000
+            });
+        }
+        /**
+         * 有1没有2
+         */
+        if(that.data.field[1] !== undefined && that.data.field[2] === undefined){
+            wx.showToast({
+                title: '请绘制防区2',
+                icon: 'none',
+                duration: 2000
+            });
+            that.setData({
+                bluedisplay:'none'
+            });
+        }
     },
     // clear:function(){
     //     var that = this;
@@ -217,13 +247,17 @@ Page({
     //     console.log("hou",that.data.dianlist);
     //
     // },
+    /**
+     * 保存
+     */
     save:function(){
         var that = this;
         /**
-         * 保存蓝色防区
+         * 两个都没有保存蓝色防区
          */
-        if(that.data.pointlist.length>0){
-            console.log("测试",that.data.pointlist.length);
+        if(that.data.pointlist.length>0 && that.data.dianlist.length === 0){
+            console.log("测试pointlist",that.data.pointlist.length);
+            console.log("测试dianlist",that.data.dianlist.length);
             const bx1 = that.data.pointlist[0][0];
             const by1 = that.data.pointlist[0][1];
             const bx2 = that.data.pointlist[1][0];
@@ -234,51 +268,72 @@ Page({
             const by4 = that.data.pointlist[5][1];
             const bfield = [[ [bx1,by1],[bx2,by2],[bx3,by3],[bx4,by4] ]];
             console.log("field",bfield);
-            request.postReq("/api/camera/fieldadd",
-                {
-                    code:'1000082',
-                    key:'1',
-                    field:bfield
-                },
-                function(res){
-                    wx.showToast({
-                        title: '防区1保存成功',
-                        icon: 'success',
-                        duration: 2000
-                    });
-                })
+            // request.postReq("/api/camera/fieldadd",
+            //     {
+            //         code:'1000082',
+            //         key:'1',
+            //         field:bfield
+            //     },
+            //     function(res){
+            //         wx.showToast({
+            //             title: '防区1保存成功',
+            //             icon: 'success',
+            //             duration: 2000
+            //         });
+            //         that.setData({
+            //             addfield:!that.data.addfield
+            //         });
+            //         /**
+            //          * 请求设备详情接口
+            //          */
+            //         request.postReq("/api/camera/getone",
+            //             {
+            //                 code:that.data.currentcode
+            //             },
+            //             function(res){
+            //                 that.setData({
+            //                     field:res.data.field
+            //                 });
+            //             });
+            //     })
         }
         /**
          * 请求增加防区接口保存红色
          */
-        // console.log("测试",that.data.dianlist.length);
-        // const x1 = that.data.dianlist[0][0];
-        // const y1 = that.data.dianlist[0][1];
-        // const x2 = that.data.dianlist[1][0];
-        // const y2 = that.data.dianlist[1][1];
-        // const x3 = that.data.dianlist[3][0];
-        // const y3 = that.data.dianlist[3][1];
-        // const x4 = that.data.dianlist[5][0];
-        // const y4 = that.data.dianlist[5][1];
-        // const field = [[ [x1,y1],[x2,y2],[x3,y3],[x4,y4] ]];
-        // console.log("field",field);
-        // request.postReq("/api/camera/fieldadd",
-        //     {
-        //         code:'1000082',
-        //         key:'2',
-        //         field:field
-        //     },
-        //     function(res){
-        //         wx.showToast({
-        //             title: '请求成功',
-        //             icon: 'success',
-        //             duration: 2000
-        //         });
-        //         // that.setData({
-        //         //     alarmListData:res.data
-        //         // })
-        //     })
+       // if(that.data.dianlist.length>0){
+       //     console.log("测试",that.data.dianlist.length);
+       //     const x1 = that.data.dianlist[0][0];
+       //     const y1 = that.data.dianlist[0][1];
+       //     const x2 = that.data.dianlist[1][0];
+       //     const y2 = that.data.dianlist[1][1];
+       //     const x3 = that.data.dianlist[3][0];
+       //     const y3 = that.data.dianlist[3][1];
+       //     const x4 = that.data.dianlist[5][0];
+       //     const y4 = that.data.dianlist[5][1];
+       //     const field = [[ [x1,y1],[x2,y2],[x3,y3],[x4,y4] ]];
+       //     console.log("field",field);
+       //     request.postReq("/api/camera/fieldadd",
+       //         {
+       //             code:'1000082',
+       //             key:'2',
+       //             field:field
+       //         },
+       //         function(res){
+       //             wx.showToast({
+       //                 title: '防区2保存成功',
+       //                 icon: 'success',
+       //                 duration: 2000
+       //             });
+       //             that.setData({
+       //                 bluedisplay:'block'
+       //             });
+       //         })
+       // }
     },
+//     /**
+//      * 点进来加载蓝色防区
+//      */
+
     /**
      * 生命周期函数--监听页面显示
      */
