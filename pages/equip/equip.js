e31fe64909442b91a90dc765cc16ba2028e0efe3
yangalong <172778564@qq.com>
2019-03-30 09:30:23 +0800

    设备分页

--- pages/equip/equip.js
+++ pages/equip/equip.js
@@ -236,10 +236,128 @@ Page({
     },
 
     /**
+     * 下拉加载
+     */
+    BottomLoad: function (condition) {
+        var ctime = util.formatTime(new Date());
+        /**
+         * 请求列表接口
+         */
+        var that = this;
+        let searchPageNum = that.data.searchPageNum,//把第几次加载次数作为参数
+            callbackcount =that.data.callbackcount; //返回数据的个数
+        request.postReq(searchPageNum,callbackcount,"/api/camera/getlist_forAPP", {},
+            function(res){
+                //判断是否有数据，有则取数据
+                if(res.data.length !== 0){
+                    let searchList = [];
+                    //如果isFromSearch是true从data中取出数据，否则先从原来的数据继续添加 equipListData
+                    console.log("isFromSearch",that.data.isFromSearch);
+                    that.data.isFromSearch ? searchList=res.data : searchList=that.data.equipListData.concat(res.data);
+                    that.setData({
+                        equipListData: searchList, //获取数据数组
+                        searchLoading: false   //把"上拉加载"的变量设为false，显示
+                    });
+                    var currenttime = new Date(ctime);
+                    for(var i=0;i<res.data.length;i++){
+                        /**
+                         *两个时间都有
+                         */
+                        if(that.data.equipListData[i].atime !== '' && that.data.equipListData[i].herattime.time !== ''){
+                            //两个时间相差的分钟数
+                            var  mistiming =  parseInt(currenttime - new Date(that.data.equipListData[i].atime))/ 1000 / 60;
+                            var  mistlastheart = parseInt(currenttime - new Date(that.data.equipListData[i].herattime.time))/ 1000 / 60;
+                            if(mistiming > 1 && mistlastheart > 1){
+                                that.data.equipListData[i]['ismist']=false;
+                                that.setData({
+                                    equipListData:that.data.equipListData
+                                })
+                            }else{
+                                that.data.equipListData[i]['ismist']=true;
+                                that.setData({
+                                    equipListData:that.data.equipListData
+                                })
+                            }
+                        }
+                        /**
+                         * 两个时间都没有
+                         */
+                        if(that.data.equipListData[i].atime === '' && that.data.equipListData[i].herattime.time === ''){
+                            that.data.equipListData[i]['ismist']=false;
+                            that.setData({
+                                equipListData:that.data.equipListData
+                            });
+                        }
+                        /**
+                         * 有报警没心跳
+                         */
+                        if(that.data.equipListData[i].atime !== '' && that.data.equipListData[i].herattime.time === ''){
+                            //两个时间相差的分钟数
+                            var  mistiming =  parseInt(currenttime - new Date(that.data.equipListData[i].atime))/ 1000 / 60;
+                            if(mistiming > 1){
+                                that.data.equipListData[i]['ismist']=false;
+                                that.setData({
+                                    equipListData:that.data.equipListData
+                                })
+                            }else{
+                                that.data.equipListData[i]['ismist']=true;
+                                that.setData({
+                                    equipListData:that.data.equipListData
+                                })
+                            }
+                        }
+                        /**
+                         * 有心跳没报警
+                         */
+                        if(that.data.equipListData[i].atime === '' && that.data.equipListData[i].herattime.time !== ''){
+                            var  mistlastheart = parseInt(currenttime - new Date(that.data.equipListData[i].herattime.time))/ 1000 / 60;
+                            if(mistlastheart > 1){
+                                that.data.equipListData[i]['ismist']=false;
+                                that.setData({
+                                    equipListData:that.data.equipListData
+                                })
+                            }else{
+                                that.data.equipListData[i]['ismist']=true;
+                                that.setData({
+                                    equipListData:that.data.equipListData
+                                })
+                            }
+                        }
+                    }
+                    // 没有数据了，把“没有数据”显示，把“上拉加载”隐藏
+                }else{
+                    that.setData({
+                        searchLoadingComplete: true, //把“没有数据”设为true，显示
+                        searchLoading: false  //把"上拉加载"的变量设为false，隐藏
+                    });
+                }
+
+            });
+    },
+    /**
      * 页面上拉触底事件的处理函数
      */
     onReachBottom: function () {
-
+        let that = this;
+        let searchPageNum = that.data.searchPageNum+1,//把第几次加载次数作为参数
+            callbackcount =that.data.callbackcount; //返回数据的个数
+        console.log("到底部了");
+        if(that.data.searchLoadingComplete === false){
+            that.setData({
+                searchLoading: true
+            });
+        }
+        console.log("加载数据",that.data.searchLoading);
+        console.log("加载完成",that.data.searchLoadingComplete);
+        if(that.data.searchLoading && !that.data.searchLoadingComplete){
+            that.setData({
+                searchPageNum: that.data.searchPageNum+1,  //每次触发上拉事件，把searchPageNum+1
+                isFromSearch: false,  //触发到上拉事件，把isFromSearch设为为false
+            });
+            that.BottomLoad();
+            console.log("that.data.searchPageNum",that.data.searchPageNum);
+            console.log("wojiu看看",that.data.searchLoading);
+        }
     },
 
     /**
