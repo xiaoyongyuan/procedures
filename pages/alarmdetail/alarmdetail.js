acb0176eecb82e752d7ffdcecae9164528abe0ad
yangalong <172778564@qq.com>
2019-04-08 16:38:56 +0800

    循环画围界报警信息

--- pages/alarmdetail/alarmdetail.js
+++ pages/alarmdetail/alarmdetail.js
@@ -32,9 +32,10 @@ Page({
      * 生命周期函数--监听页面加载
      */
     onLoad: function (options) {
-        var that= this
+        var that= this;
         // 字符串转json
         const code = options.code;
+
         that.setData({
             currentcode:code
         });
@@ -75,90 +76,47 @@ Page({
                 that.setData({
                     alarmdetailData:res.data,
                 });
-                //一个围界信息
+                //围界信息
+                var percentx = that.data.alarmdetailimgWidth/704;
+                var percenty = that.data.alarmdetailimgHeight/576;
                 if(that.data.alarmdetailData.field.length > 0){
-                    const x1 = that.data.alarmdetailData.field[0][0][0];
-                    const y1 = that.data.alarmdetailData.field[0][0][1];
-                    const x2 = that.data.alarmdetailData.field[0][1][0];
-                    const y2 = that.data.alarmdetailData.field[0][1][1];
-                    const x3 = that.data.alarmdetailData.field[0][2][0];
-                    const y3 = that.data.alarmdetailData.field[0][2][1];
-                    const x4 = that.data.alarmdetailData.field[0][3][0];
-                    const y4 = that.data.alarmdetailData.field[0][3][1];
                     myred_carvas = wx.createCanvasContext('redCanvas', this);//1.创建carvas实例对象，方便后续使用。
                     myred_carvas.beginPath(); //创建一条路径
                     myred_carvas.setStrokeStyle('red');  //设置边框为红色myblueCanvas
                     myred_carvas.setLineWidth(2);
-                    myred_carvas.moveTo(x1*(that.data.alarmdetailimgWidth/704),y1*(that.data.alarmdetailimgHeight/576)); //描述路径的起点为手指触摸的x轴和y轴
-                    myred_carvas.lineTo(x2*(that.data.alarmdetailimgWidth/704),y2*(that.data.alarmdetailimgHeight/576));//绘制一条直线，终点坐标为手指触摸结束后的x轴和y轴
-                    myred_carvas.lineTo(x3*(that.data.alarmdetailimgWidth/704),y3*(that.data.alarmdetailimgHeight/576));
-                    myred_carvas.lineTo(x4*(that.data.alarmdetailimgWidth/704),y4*(that.data.alarmdetailimgHeight/576));
-                    myred_carvas.lineTo(x1*(that.data.alarmdetailimgWidth/704),y1*(that.data.alarmdetailimgHeight/576));
+                    for(var i = 0;i < that.data.alarmdetailData.field.length;i++){
+                        const x1 = that.data.alarmdetailData.field[i][0][0];
+                        const y1 = that.data.alarmdetailData.field[i][0][1];
+                        const x2 = that.data.alarmdetailData.field[i][1][0];
+                        const y2 = that.data.alarmdetailData.field[i][1][1];
+                        const x3 = that.data.alarmdetailData.field[i][2][0];
+                        const y3 = that.data.alarmdetailData.field[i][2][1];
+                        const x4 = that.data.alarmdetailData.field[i][3][0];
+                        const y4 = that.data.alarmdetailData.field[i][3][1];
+                        myred_carvas.moveTo(x1*percentx,y1*percenty); //描述路径的起点为手指触摸的x轴和y轴
+                        myred_carvas.lineTo(x2*percentx,y2*percenty);//绘制一条直线，终点坐标为手指触摸结束后的x轴和y轴
+                        myred_carvas.lineTo(x3*percentx,y3*percenty);
+                        myred_carvas.lineTo(x4*percentx,y4*percenty);
+                        myred_carvas.lineTo(x1*percentx,y1*percenty);
+                    }
                     myred_carvas.stroke();//画出当前路径的边框
                     myred_carvas.draw(); //将之前在绘图上下文中的描述（路径、变形、样式）画到 canvas 中。
                 }
-                //两个围界信息
-                if(that.data.alarmdetailData.field.length > 1){
-                    //第一个围界的点
-                    const x1 = that.data.alarmdetailData.field[0][0][0];
-                    const y1 = that.data.alarmdetailData.field[0][0][1];
-                    const x2 = that.data.alarmdetailData.field[0][1][0];
-                    const y2 = that.data.alarmdetailData.field[0][1][1];
-                    const x3 = that.data.alarmdetailData.field[0][2][0];
-                    const y3 = that.data.alarmdetailData.field[0][2][1];
-                    const x4 = that.data.alarmdetailData.field[0][3][0];
-                    const y4 = that.data.alarmdetailData.field[0][3][1];
-                    //第二个围界的点
-                    const x11 = that.data.alarmdetailData.field[1][0][0];
-                    const y11 = that.data.alarmdetailData.field[1][0][1];
-                    const x21 = that.data.alarmdetailData.field[1][1][0];
-                    const y21 = that.data.alarmdetailData.field[1][1][1];
-                    const x31 = that.data.alarmdetailData.field[1][2][0];
-                    const y31 = that.data.alarmdetailData.field[1][2][1];
-                    const x41 = that.data.alarmdetailData.field[1][3][0];
-                    const y41 = that.data.alarmdetailData.field[1][3][1];
-                    myred_carvas = wx.createCanvasContext('redCanvas', this);//1.创建carvas实例对象，方便后续使用。
-                    myred_carvas.beginPath(); //创建一条路径
-                    myred_carvas.setStrokeStyle('red');  //设置边框为红色myblueCanvas
-                    myred_carvas.setLineWidth(2);
-                    //第一个
-                    myred_carvas.moveTo(x1*(that.data.alarmdetailimgWidth/704),y1*(that.data.alarmdetailimgHeight/576)); //描述路径的起点为手指触摸的x轴和y轴
-                    myred_carvas.lineTo(x2*(that.data.alarmdetailimgWidth/704),y2*(that.data.alarmdetailimgHeight/576));//绘制一条直线，终点坐标为手指触摸结束后的x轴和y轴
-                    myred_carvas.lineTo(x3*(that.data.alarmdetailimgWidth/704),y3*(that.data.alarmdetailimgHeight/576));
-                    myred_carvas.lineTo(x4*(that.data.alarmdetailimgWidth/704),y4*(that.data.alarmdetailimgHeight/576));
-                    myred_carvas.lineTo(x1*(that.data.alarmdetailimgWidth/704),y1*(that.data.alarmdetailimgHeight/576));
-                    //第二个
-                    myred_carvas.moveTo(x11*(that.data.alarmdetailimgWidth/704),y11*(that.data.alarmdetailimgHeight/576)); //描述路径的起点为手指触摸的x轴和y轴
-                    myred_carvas.lineTo(x21*(that.data.alarmdetailimgWidth/704),y21*(that.data.alarmdetailimgHeight/576));//绘制一条直线，终点坐标为手指触摸结束后的x轴和y轴
-                    myred_carvas.lineTo(x31*(that.data.alarmdetailimgWidth/704),y31*(that.data.alarmdetailimgHeight/576));
-                    myred_carvas.lineTo(x41*(that.data.alarmdetailimgWidth/704),y41*(that.data.alarmdetailimgHeight/576));
-                    myred_carvas.lineTo(x11*(that.data.alarmdetailimgWidth/704),y11*(that.data.alarmdetailimgHeight/576));
-                    myred_carvas.stroke();//画出当前路径的边框
-                    myred_carvas.draw(); //将之前在绘图上下文中的描述（路径、变形、样式）画到 canvas 中。
-                }
+                //报警信息
                 if(that.data.alarmdetailData.finalresult1.length > 0){
-                   // for(var i = 0;i < that.data.alarmdetailData.finalresult1.length;i++){
-                       const x = that.data.alarmdetailData.finalresult1[0].x;
-                       const y = that.data.alarmdetailData.finalresult1[0].y;
-                       const w = that.data.alarmdetailData.finalresult1[0].w;
-                       const h = that.data.alarmdetailData.finalresult1[0].h;
-                       //
-                       // const x1 = that.data.alarmdetailData.finalresult1[1].x;
-                       // const y1 = that.data.alarmdetailData.finalresult1[1].y;
-                       // const w1 = that.data.alarmdetailData.finalresult1[1].w;
-                       // const h1 = that.data.alarmdetailData.finalresult1[1].h;
-
-                       const percentx = that.data.alarmdetailimgWidth/that.data.alarmdetailData.pic_width;
-                       const percenty = that.data.alarmdetailimgHeight/that.data.alarmdetailData.pic_height;
-                       myblue_carvas = wx.createCanvasContext('blueCanvas', this);//1.创建carvas实例对象，方便后续使用。
-                       myblue_carvas.setStrokeStyle('#00e8e8');
-                       myblue_carvas.setLineWidth(2);
-                       myblue_carvas.strokeRect(x*percentx, y*percenty, w*percentx, h*percenty);
-                       // myblue_carvas.strokeRect(x*percentx, y*percenty, w*percentx, h*percenty);
-                       // myblue_carvas.strokeRect(x1*percentx, y1*percenty, w1*percentx, h1*percenty);
-                       // myblue_carvas.strokeRect(that.data.alarmdetailData.finalresult1[i].x*percentx, that.data.alarmdetailData.finalresult1[i].y*percenty, that.data.alarmdetailData.finalresult1[i].w*percentx, that.data.alarmdetailData.finalresult1[i].h*percenty);
-                       myblue_carvas.draw();
-                   // }
+                    var percentx = that.data.alarmdetailimgWidth/that.data.alarmdetailData.pic_width;
+                    var percenty = that.data.alarmdetailimgHeight/that.data.alarmdetailData.pic_height;
+                    myblue_carvas = wx.createCanvasContext('blueCanvas', this);//1.创建carvas实例对象，方便后续使用。
+                    myblue_carvas.setStrokeStyle('#00e8e8');
+                    myblue_carvas.setLineWidth(2);
+                    for(var i = 0;i < that.data.alarmdetailData.finalresult1.length; i++){
+                        var x = that.data.alarmdetailData.finalresult1[i].x;
+                        var y = that.data.alarmdetailData.finalresult1[i].y;
+                        var w = that.data.alarmdetailData.finalresult1[i].w;
+                        var h = that.data.alarmdetailData.finalresult1[i].h;
+                        myblue_carvas.strokeRect(x*percentx, y*percenty, w*percentx, h*percenty);
+                    }
+                    myblue_carvas.draw();
                 }
             })
     },
