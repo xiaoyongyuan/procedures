de3ba3f58b35623c0cb634548c2abc32b0340749
yangalong <172778564@qq.com>
2019-04-25 17:45:24 +0800

    一键处理

--- pages/index/index.js
+++ pages/index/index.js
@@ -8,26 +8,85 @@ Page({
         userInfo: null,
         title:'个人中心',
         showModal: false,
+        showModalalarm:false,
+        showModSF:false,
+        showModalHC:false,
+        alarmequiplist: [],
+        SFequiplist:[],
+        items: [
+            {name: 'USA', value: '美国'},
+            {name: 'CHN', value: '中国', checked: 'true'},
+            {name: 'BRA', value: '巴西'},
+            {name: 'JPN', value: '日本'},
+            {name: 'ENG', value: '英国'},
+            {name: 'TUR', value: '法国'},
+        ]
     },
     submit: function () {
         this.setData({
-            showModal: true
+            showModal: true,
+            showModalHC:true
         })
     },
+    checkboxChange(e) {
+        var that = this;
+        console.log('checkbox发生change事件，携带value值为：', e.detail.value);
+        that.setData({
+            keySFcid:e.detail.value
+        });
+        console.log("keySFcid",that.data.keySFcid.join(','));
+    },
+    radioChange(e) {
+        var that = this;
+        console.log('radio发生change事件，携带value值为：', e.detail.value);
+        that.setData({
+            keyalarmcid:e.detail.value
+        });
 
+    },
     preventTouchMove: function () {
 
     },
     go: function () {
         this.setData({
-            showModal: false
+            showModal: false,
+            showModalalarm: false,
+            showModalHC:false,
+            showModSF:false,
+            keyalarmcid:''
         })
     },
+    alarmselect:function(e){
+        var that = this;
+        for(var i=0;i<that.data.alarmequiplist.length;i++){
+            that.data.alarmequiplist[i]['isselect']=false;
+        }
+        that.setData({
+            alarmequiplist:that.data.alarmequiplist
+        });
+        //获取当前点击元素的id(索引值)
+        var Id = e.currentTarget.id;
+        console.log("ID",Id);
+        // var isselect = that.data.alarmequiplist[Id].isselect;
+        // console.log("isselect",isselect);
+        that.data.alarmequiplist[Id]['isselect']=true;
+        console.log("that.data.alarmequiplist[Id]['isselect']",that.data.alarmequiplist[Id]['isselect']);
+        that.setData({
+            alarmequiplist:that.data.alarmequiplist,
+            keyalarmcid:that.data.alarmequiplist[Id]['code']
+        });
+        console.log("keyalarmcid",that.data.keyalarmcid);
+        console.log("alarmequiplist",that.data.alarmequiplist);
+    },
+    /**
+     * 清除缓存
+     */
     openToast: function () {
         wx.clearStorageSync();
         wx.clearStorage();
         this.setData({
             showModal: false,
+            showModalHC:false,
             currentSize:0,
         });
         wx.showToast({
@@ -35,6 +94,116 @@ Page({
             icon: 'success',
             duration: 2000
         });
+    },
+    /**
+     * 一键报警设备
+     */
+    keyalarm:function(){
+        this.setData({
+            showModalalarm: true,
+            showModal:true
+        });
+        var that = this;
+        console.log("keyalarmcid",that.data.keyalarmcid);
+        /**
+         * 请求设备列表接口
+         */
+        request.postReq('','',"/api/camera/getlist_working",
+            {
+                alarm:1
+            },
+            function(res){
+            console.log("res",res);
+                that.setData({
+                    alarmequiplist:res.data,
+                });
+                for(var i = 0;i<res.data.length;i++){
+                    that.data.alarmequiplist[i]['isselect']=false;
+                    that.setData({
+                        alarmequiplist:that.data.alarmequiplist
+                    })
+                }
+            });
+    },
+    /**
+     * 一键设防设备
+     */
+    keySF:function(){
+        var that = this;
+        that.setData({
+            showModal:true,
+            showModSF:true
+        });
+        var that = this;
+        console.log("keySFcid",that.data.keySFcid);
+        /**
+         * 请求设备列表接口
+         */
+        request.postReq('','',"/api/camera/getlist_working",
+            {
+                if_cancel:1
+            },
+            function(res){
+                console.log("res",res);
+                that.setData({
+                    SFequiplist:res.data,
+                });
+            });
+    },
+    /**
+     * 一键报警确认
+     */
+    surekeyalarm:function(){
+        var that = this;
+        if(that.data.keyalarmcid === undefined ||  that.data.keyalarmcid ===''){
+            wx.showToast({
+                title: '请选择设备！',
+                icon: 'none',
+                duration: 2000
+            });
+            return
+        }
+        if(that.data.keyalarmcid !== undefined &&  that.data.keyalarmcid !==''){
+            this.setData({
+                showModalalarm: false,
+                showModal:false
+            });
+            /**
+             * 一键报警接口
+             */
+            request.postReq('','',"/api/alarm/givealarm",
+                {
+                    cid:that.data.keyalarmcid
+                },
+                function(res){
+                    console.log("res",res);
+                    if(res.success === 1){
+                        wx.showToast({
+                            title: '一键报警成功！',
+                            icon: 'success',
+                            duration: 2000
+                        });
+                    }
+                    that.setData({
+                        keyalarmcid:''
+                    });
+                });
+        }
+    },
+    /**
+     * 一键设防确认
+     */
+    surekeySF:function(){
+        var that = this;
+        console.log("keySFcid",that.data.keySFcid);
+        if(that.data.keySFcid === undefined ||  that.data.keySFcid ===''){
+            wx.showToast({
+                title: '请选择设备！',
+                icon: 'none',
+                duration: 2000
+            });
+            return
+        }
     },
     /**
      * 生命周期函数--监听页面加载
